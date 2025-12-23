df3ad27456d65cce43e0e6091dc08101d7c40c77
narekk1202 <narekdunamaliiian@gmail.com>
2025-12-23 16:43:02 +0400

    style: improve code formatting and consistency
    Refactor the route handlers for better readability by removing unnecessary parentheses and correcting the response key for the room creation endpoint.

--- src/routes/rooms.route.ts
+++ src/routes/rooms.route.ts
@@ -5,7 +5,7 @@ import { roomValidation } from '../types/validations/room.validations';
 
 const app = new Hono();
 
-app.get('/', async (c) => {
+app.get('/', async c => {
 	try {
 		const rooms = await roomsService.getAll();
 		return c.json({ rooms });
@@ -13,13 +13,13 @@ app.get('/', async (c) => {
 		console.error('Error fetching rooms:', error);
 		return c.json({ error: 'Failed to fetch rooms' }, 500);
 	}
-})
+});
 
 app.post('/create', zValidator('json', roomValidation), async c => {
 	const { name } = c.req.valid('json');
 	try {
 		const newRoom = await roomsService.createRoom(name);
-		return c.json({ room: newRoom });
+		return c.json({ newRoom });
 	} catch (error) {
 		console.error('Error creating room:', error);
 		return c.json({ error: 'Failed to create room' }, 500);
